1861b2473f629bfe77ff9aaa4b0ac304c0bdc716
John Ryding <john.ryding@inin.com>
2014-10-25 16:50:03 -0400

    Set up grunt to run local server
    - Set up Gruntfile.js to run local server, compile LESS files, and run jshint
    - Configure JSHint
    - Add README.md to explain how to set up the project

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    // Load grunts tasks from package.json
+    require('load-grunt-tasks')(grunt);
+
+    // =========================================================
+    // Task Configuration
+    // =========================================================
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    base: 'src',
+                    port: 9000
+                }
+            }
+        },
+        jshint: {
+            app: {
+                options: {
+                    jshintrc: 'src/.jshintrc'
+                },
+                files: {
+                    src: ['src/**/*.js', '!src/vendor/**/*.js']
+                }
+            },
+        },
+        less: {
+            dev: {
+                files: {
+                    'src/style.css': 'src/style.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: 'src/**/*.less',
+                tasks: 'less:dev',
+                interrupt: true
+            },
+            jshint: {
+              files: ['<%= jshint.app.files.src %>'],
+              tasks: ['jshint:app']
+            }
+        },
+    });
+
+    // =========================================================
+    // Task Definition
+    // =========================================================
+
+    grunt.registerTask('server', function() {
+        grunt.task.run([
+            'less:dev',
+            'connect:server',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('default', ['server']);
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# Dependencies
+
+* [Node.js]()
+* [npm]()
+* [Grunt]()
+
+# Setup
+
+1. Open a command line and set the terminal to the root of this project.
+2. Run ```npm install```
+3. Run ```grunt server```
+4. Open a browser to http://localhost:9000
