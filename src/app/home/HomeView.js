e0e48fd6bd250b42b3177ba5cb73e0a514baef09
John Ryding <john.ryding@inin.com>
2014-10-29 00:03:43 -0400

    Added sub navigation to the page

--- src/app/home/HomeView.js
+++ src/app/home/HomeView.js
@@ -10,6 +10,24 @@ define([
 
         self.template = "app/home/home";
 
-        self.taco = ko.observable("chalupa home");
+        self.selectedLayout = ko.observable("l-list");
+
+        self.selectedFilter = ko.observable("all");
+
+        //
+        // Event Callbacks
+        //
+
+        self.displayAllPosts = function () {
+            self.selectedFilter("all");
+        };
+
+        self.displayPhotoPosts = function () {
+            self.selectedFilter("photos");
+        };
+
+        self.displayVideoPosts = function () {
+            self.selectedFilter("videos");
+        };
     };
 });
