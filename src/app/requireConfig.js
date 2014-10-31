8920248316b0a9f5c20f02ac60733c2d9092e9a7
John Ryding <john.ryding@inin.com>
2014-10-30 21:59:31 -0400

    Fix cross browser bugs
    - IE needs SVG objects to have both height and width defined to scale them correctly

--- src/app/requireConfig.js
+++ src/app/requireConfig.js
@@ -7,7 +7,7 @@
             'lodash':        'vendor/js/lodash',
             'jquery':        'vendor/js/jquery-2.1.1.min',
             'knockout':      'vendor/js/knockout-3.2.0',
-            'ko.amdHelpers': 'vendor/js/knockout-amd-helpers',
+            'ko.amdHelpers': 'vendor/js/knockout-amd-helpers.min',
             'sammy':         'vendor/js/sammy',
             'text':          'vendor/js/text',
         },
